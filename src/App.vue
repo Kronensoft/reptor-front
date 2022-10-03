23e355c2ca60ec3422bc14d3a99d367b2a63e8db
Igor Kroeber <igor.kroeber@protonmail.com>
2022-10-02 23:06:59 -0300

    Initial project template

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <Header />
+  <Content />
+</template>
+
+<script setup>
+import Content from './components/Content.vue';
+import Header from './components/Header.vue';
+</script>
+
+
+<style>
+html,
+body,
+#app {
+  display: flex;
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+h1 {
+  font-size: 32px;
+}
+
+h2 {
+  font-size: 24px;
+}
+
+h3 {
+  font-size: 20px;
+}
+
+ul {
+  list-style: disc;
+  padding-left: 25px;
+}
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,47 @@
+<template>
+  <aside>
+    <div>
+      <h1>Reptor</h1>
+      <h2>Web Reporting Engine</h2>
+    </div>
+    <div>
+      <div>
+        <!-- <img /> -->
+      </div>
+      <div>
+        <span></span>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li><a href="_blank">Reports</a></li>
+        <li><a href="_blank">Users</a></li>
+        <li><a href="_blank">Configuration</a></li>
+      </ul>
+    </nav>
+  </aside>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+aside {
+  box-sizing: border-box;
+  height: 100%;
+  width: 300px;
+  color: #fafafa;
+  background-color: #3498DB;
+  padding: 0 1em 0 1em;
+}
+
+ul {
+  list-style-type: none;
+  padding-left: 0;
+}
+
+ul a {
+  color: #fafafa;
+  text-decoration: none;
+}
+</style>
